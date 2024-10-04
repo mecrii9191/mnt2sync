0c51442aff8224bd64c357d576f5084b9510051e
mecrii9191 <55257186+mecrii9191@users.noreply.github.com>
2024-10-05 01:16:20 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,63 +1,73 @@
 const sendWarn = (msg, type) => {
-	const message = document.querySelector('#message');
-	switch (type) {
-		case 0:
-			message.className = 'errorLabel';
-			break;
-		case 1:
-			message.className = 'successLabel';
-			break;
-		default:
-			break;
-	}
-	
-	message.innerText = msg;
+    const message = document.querySelector('#message');
+    switch (type) {
+        case 0:
+            message.className = 'errorLabel';
+            break;
+        case 1:
+            message.className = 'successLabel';
+            break;
+        default:
+            break;
+    }
+
+    message.innerText = msg;
 }
 
 (async () => { // global data
-	await fetch('/api/v1/syncs');
-	const { attempts, volumeMap } = await fetch('/api/v1/globalData').then((res) => res.json());
-	console.log('load', attempts, volumeMap);
-	
-	if (attempts > 0) {
-		sendWarn(`invalid attempts: ${globalData.attempts}`, 0);
-	}
-	
-	const keys = Object.keys(volumeMap);
+    await fetch('/api/v1/syncs');
+    const {
+        attempts,
+        partitions
+    } = await fetch('/api/v1/globalData').then((res) => res.json());
+    console.log('load', attempts, partitions);
 
-	if (keys.length > 0) {
-		const dropdown = document.querySelector('select');
-		keys.forEach((key) => {
-			const opt = document.createElement('option');
-			opt.value = key;
-			opt.innerHTML = key.replace('/dev/mapper', '');
-			dropdown.appendChild(opt);
-		});
-		// add options...
-	}
+    if (attempts > 0) {
+        sendWarn(`invalid attempts: ${globalData.attempts}`, 0);
+    }
+
+    if (partitions.length > 0) {
+        const dropdown = document.querySelector('select');
+        partitions.forEach((key) => {
+            const opt = document.createElement('option');
+            opt.value = key;
+            opt.innerHTML = key.replace('/dev/mapper', '');
+            dropdown.appendChild(opt);
+        });
+        // add options...
+    }
 })();
 
 const mount = (s, pas) => {
-	return fetch('/api/v1/mount', {
-		method: 'POST',
-		body: JSON.stringify({
-			sync: s,
-			password: pas
-		})
-	}).then((res) => res.json());
+    return fetch('/api/v1/mount', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            sync: s,
+            password: pas
+        })
+    }).then((res) => res.json());
 }
 
 window.mog = async () => {
-	const selected = document.querySelector('#type').value;
-	const password = document.querySelector('input[type=password]').value;
-	
-	try {
-		const res = await mount(selected, password);
-		console.log(res);
-		sendWarn('mounted sync!', 1);
-	}
-	catch (err) {
-		console.log(err.message);
-		sendWarn('failed to mount sync! (invalid password?)', 0);
-	}
+    const selected = document.querySelector('#type').value;
+    const password = document.querySelector('input[type=password]').value;
+
+    try {
+        const res = await mount(selected, password);
+        console.log(res);
+        if (res.mounted) {
+            sendWarn('mounted sync!', 1);
+        } else {
+            sendWarn(`failed to mount sync! (${res.message})`, 0);
+        }
+    } catch (err) {
+        const {
+            message
+        } = err.message;
+        sendWarn(`failed to mount sync! (${message})`, 0);
+    }
 };
